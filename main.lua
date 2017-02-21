0fb2cba9b306ec8a2174078e7d3ac9aac7c3b825
powerboat9 <powerboat9@users.noreply.github.com>
2017-02-20 23:47:04 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,8 @@
-local function transmit(modem, msg, key, mode)
-    local 
+local function transmit(modem, msg, p, key, mode)
+    local m = {}
+
+    if m == "AES" or m == 0 
+        if type(key) ~= "string" or (#key ~= 16 and #key ~= 32) then
+            error("Invalid AES keykey", 2)
+        else
+            if 
